4ff3aa2ad66a3e2a9180668c956cbe23528cc266
Gregory <its.crowd@gmail.com>
2019-02-05 17:51:41 +0300

    fix

--- main.go
+++ main.go
@@ -45,7 +45,8 @@ func main() {
 
 	pubsub := srv.Server().Options().Broker
 
-	fmt.Println(pubsub.Options())
+	fmt.Println(pubsub.Options().Codec)
+
 
 	// Register handler
 	pb.RegisterUserServiceHandler(srv.Server(), &service{repo, tokenService, pubsub})
