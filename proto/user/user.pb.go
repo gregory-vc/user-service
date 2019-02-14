7c9403c1abfce5728421aa58141d60bdc235b30b
Gregory <its.crowd@gmail.com>
2019-02-14 17:15:24 +0300

    fix

--- proto/user/user.pb.go
+++ proto/user/user.pb.go
@@ -8,6 +8,12 @@ import fmt "fmt"
 import math "math"
 import timestamp "github.com/golang/protobuf/ptypes/timestamp"
 
+import (
+	client "github.com/micro/go-micro/client"
+	server "github.com/micro/go-micro/server"
+	context "golang.org/x/net/context"
+)
+
 // Reference imports to suppress errors if they are not otherwise used.
 var _ = proto.Marshal
 var _ = fmt.Errorf
@@ -311,6 +317,127 @@ func init() {
 	proto.RegisterType((*Response)(nil), "user.Response")
 	proto.RegisterType((*Token)(nil), "user.Token")
 	proto.RegisterType((*Error)(nil), "user.Error")
+}
+
+// Reference imports to suppress errors if they are not otherwise used.
+var _ context.Context
+var _ client.Option
+var _ server.Option
+
+// Client API for UserService service
+
+type UserServiceClient interface {
+	Create(ctx context.Context, in *User, opts ...client.CallOption) (*Response, error)
+	Get(ctx context.Context, in *User, opts ...client.CallOption) (*Response, error)
+	GetAll(ctx context.Context, in *Request, opts ...client.CallOption) (*Response, error)
+	Auth(ctx context.Context, in *User, opts ...client.CallOption) (*Token, error)
+	ValidateToken(ctx context.Context, in *Token, opts ...client.CallOption) (*Token, error)
+}
+
+type userServiceClient struct {
+	c           client.Client
+	serviceName string
+}
+
+func NewUserServiceClient(serviceName string, c client.Client) UserServiceClient {
+	if c == nil {
+		c = client.NewClient()
+	}
+	if len(serviceName) == 0 {
+		serviceName = "user"
+	}
+	return &userServiceClient{
+		c:           c,
+		serviceName: serviceName,
+	}
+}
+
+func (c *userServiceClient) Create(ctx context.Context, in *User, opts ...client.CallOption) (*Response, error) {
+	req := c.c.NewRequest(c.serviceName, "UserService.Create", in)
+	out := new(Response)
+	err := c.c.Call(ctx, req, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *userServiceClient) Get(ctx context.Context, in *User, opts ...client.CallOption) (*Response, error) {
+	req := c.c.NewRequest(c.serviceName, "UserService.Get", in)
+	out := new(Response)
+	err := c.c.Call(ctx, req, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *userServiceClient) GetAll(ctx context.Context, in *Request, opts ...client.CallOption) (*Response, error) {
+	req := c.c.NewRequest(c.serviceName, "UserService.GetAll", in)
+	out := new(Response)
+	err := c.c.Call(ctx, req, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *userServiceClient) Auth(ctx context.Context, in *User, opts ...client.CallOption) (*Token, error) {
+	req := c.c.NewRequest(c.serviceName, "UserService.Auth", in)
+	out := new(Token)
+	err := c.c.Call(ctx, req, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *userServiceClient) ValidateToken(ctx context.Context, in *Token, opts ...client.CallOption) (*Token, error) {
+	req := c.c.NewRequest(c.serviceName, "UserService.ValidateToken", in)
+	out := new(Token)
+	err := c.c.Call(ctx, req, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+// Server API for UserService service
+
+type UserServiceHandler interface {
+	Create(context.Context, *User, *Response) error
+	Get(context.Context, *User, *Response) error
+	GetAll(context.Context, *Request, *Response) error
+	Auth(context.Context, *User, *Token) error
+	ValidateToken(context.Context, *Token, *Token) error
+}
+
+func RegisterUserServiceHandler(s server.Server, hdlr UserServiceHandler, opts ...server.HandlerOption) {
+	s.Handle(s.NewHandler(&UserService{hdlr}, opts...))
+}
+
+type UserService struct {
+	UserServiceHandler
+}
+
+func (h *UserService) Create(ctx context.Context, in *User, out *Response) error {
+	return h.UserServiceHandler.Create(ctx, in, out)
+}
+
+func (h *UserService) Get(ctx context.Context, in *User, out *Response) error {
+	return h.UserServiceHandler.Get(ctx, in, out)
+}
+
+func (h *UserService) GetAll(ctx context.Context, in *Request, out *Response) error {
+	return h.UserServiceHandler.GetAll(ctx, in, out)
+}
+
+func (h *UserService) Auth(ctx context.Context, in *User, out *Token) error {
+	return h.UserServiceHandler.Auth(ctx, in, out)
+}
+
+func (h *UserService) ValidateToken(ctx context.Context, in *Token, out *Token) error {
+	return h.UserServiceHandler.ValidateToken(ctx, in, out)
 }
 
 func init() { proto.RegisterFile("proto/user/user.proto", fileDescriptor_user_44df9c3e671c23ab) }
