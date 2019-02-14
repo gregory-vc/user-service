4dd03f289bc26b28c662a059b6bd23c526d9a9f6
Gregory <its.crowd@gmail.com>
2019-02-14 19:18:09 +0300

    fix

--- handler.go
+++ handler.go
@@ -29,6 +29,7 @@ func (srv *service) Get(ctx context.Context, req *pb.User, res *pb.Response) err
 
 func (srv *service) GetAll(ctx context.Context, req *pb.Request, res *pb.Response) error {
 	users, err := srv.repo.GetAll()
+	fmt.Println(users)
 	if err != nil {
 		return err
 	}
